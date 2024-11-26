61852c6caea2eb9b3e6c934ac3fc22fa8950ad16
gilan <gilan@LAPTOP-UQ6NP8VU>
2024-11-26 10:46:43 +0700

    Alpro

---
+++ src/Pekan1/Helloworld.java
@@ -0,0 +1,15 @@
+package Pekan1;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println();
+		System.out.println("Program ini menghasilkan");
+		System.out.println("Empat baris output");
+		System.out.println("/ \\ // \\\\ /// \\\\");
+		System.out.println("This"+"Program prints a/n"+"quote from the Gettysburg Address");
+        
+	}
+
+}
---
+++ src/Pekan6/perulanganwhile1.java
@@ -0,0 +1,28 @@
+package Pekan6;
+
+import java.util.Scanner;
+
+public class perulanganwhile1 {
+
+	public static void main(String[] args) {
+	     int counter=0;
+	     String jawab;
+	     boolean running = true;
+	     //deklarasi scanner
+	      
+	     Scanner scan = new Scanner (System.in);
+	     while (running) {
+	    	 counter++;
+	    	 System.out.println("Jumlah = "+counter);
+	    	 System.out.println("Apakah lanjut (ya / tidak?");
+	    	 jawab= scan.nextLine();
+	    	 //cek jawab, jika tidak maka perulangan berhenti
+	    	 if (jawab.equalsIgnoreCase("tidak")) {
+	    		 running= false;
+	    				 
+	    	 }
+	     }
+             System.out.println("Anda sudah melakukan perulangan sebanyak :"+ counter +"kali");
+	}
+
+}
